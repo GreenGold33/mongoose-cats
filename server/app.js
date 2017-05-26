ddb7a9573887a8843938773aac937bf52193320d
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-26 13:17:30 +0200

    last changes

--- server/app.js
+++ server/app.js
@@ -6,16 +6,19 @@ const path = require('path')
 const routerCats = require('./routes/cats')
 const routerCat = require('./routes/cat')
 
-const dbUrl = 'mongodb://localhost:27017/test'
-const PORT = 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const dbUrl = process.env.DB_URL
+const PORT = process.env.PORT
 
 const app = express()
 
 mongoose.Promise = Promise
 mongoose.connect(dbUrl)
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
--- server/routes/cats/handlers/getAll.js
+++ server/routes/cats/handlers/getAll.js
@@ -1,15 +1,13 @@
 const Cat = require('../../../models/Cat')
 const debug = require('debug')('cats')
 
-function getAll(req,res) {
-
-  const title = "My favourite Cats"
+function getAll (req, res) {
+  const title = 'My favourite Cats'
   Cat.find()
-    .then( cats => {
+    .then(cats => {
       debug('succesfully got all cats...')
-      res.render('list', { cats, title })
+      res.json(cats)
     })
-
 }
 
 module.exports = getAll
